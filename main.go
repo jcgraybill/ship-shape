177ac2f677b98168781a4f76754ee42dcbc9f68d
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2022-04-11 21:11:30 -0700

    starfield, shapes, type

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"image"
 	"image/color"
+	"math/rand"
+	"os"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
 
 const (
-	w = 640
-	h = 480
+	w          = 1024
+	h          = 768
+	starriness = 3000
+	ttf        = "Urbanist-Regular.ttf"
+)
+
+var (
+	emptyImage = ebiten.NewImage(3, 3)
 )
 
 type Game struct {
-	bg *ebiten.Image
+	bg  *ebiten.Image
+	ttf font.Face
 }
 
 func (g *Game) Update() error {
@@ -21,6 +35,21 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(g.bg, nil)
+	src := emptyImage.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image)
+	v, i := rectangle(50, 50, 120, 120, color.RGBA{0x00, 0x80, 0x00, 0xff})
+	screen.DrawTriangles(v, i, src, nil)
+	v, i = circle(120, 300, 60, color.RGBA{0x80, 0x00, 0x00, 0xff})
+	screen.DrawTriangles(v, i, src, nil)
+	v, i = line(400, 100, 600, 200, 2, color.RGBA{0x00, 0x00, 0xff, 0xff})
+	screen.DrawTriangles(v, i, src, nil)
+	v, i = triangle(400, 200, 100, 100, color.RGBA{0xff, 0x00, 0xff, 0xff})
+	screen.DrawTriangles(v, i, src, nil)
+	text.Draw(screen, "hello world", g.ttf, 20, 40, color.White)
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	emptyImage.Fill(color.White)
 }
 
 func main() {
@@ -29,9 +58,10 @@ func main() {
 
 	image := ebiten.NewImage(w, h)
 	image.Fill(color.Black)
-
+	starField(image)
 	g := Game{
-		bg: image,
+		bg:  image,
+		ttf: loadFont(ttf),
 	}
 
 	if err := ebiten.RunGame(&g); err != nil {
@@ -41,4 +71,40 @@ func main() {
 
 func (g *Game) Layout(int, int) (int, int) {
 	return w, h
+}
+
+func starField(image *ebiten.Image) {
+	star := ebiten.NewImage(2, 2)
+
+	for y := 0; y < image.Bounds().Dy(); y++ {
+		for x := 0; x < image.Bounds().Dx(); x++ {
+			if rand.Intn(starriness) == 0 {
+				hue := uint8(rand.Intn(255))
+				star.Fill(color.RGBA{hue, hue, hue, 255})
+				opts := &ebiten.DrawImageOptions{}
+				opts.GeoM.Translate(float64(x), float64(y))
+				image.DrawImage(star, opts)
+			}
+		}
+	}
+}
+
+func loadFont(path string) font.Face {
+	ttbytes, err := os.ReadFile(path)
+	if err == nil {
+		tt, err := opentype.Parse(ttbytes)
+		if err == nil {
+			fontface, err := opentype.NewFace(tt, &opentype.FaceOptions{
+				Size:    24,
+				DPI:     72,
+				Hinting: font.HintingFull,
+			})
+			if err == nil {
+				return fontface
+			}
+			panic(err)
+		}
+		panic(err)
+	}
+	panic(err)
 }
---
+++ shapes.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"image/color"
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+func rectangle(x, y, w, h float32, clr color.RGBA) ([]ebiten.Vertex, []uint16) {
+	r := float32(clr.R) / 0xff
+	g := float32(clr.G) / 0xff
+	b := float32(clr.B) / 0xff
+	a := float32(clr.A) / 0xff
+	x0 := x
+	y0 := y
+	x1 := x + w
+	y1 := y + h
+
+	return []ebiten.Vertex{
+		{
+			DstX:   x0,
+			DstY:   y0,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+		{
+			DstX:   x1,
+			DstY:   y0,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+		{
+			DstX:   x0,
+			DstY:   y1,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+		{
+			DstX:   x1,
+			DstY:   y1,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+	}, []uint16{0, 1, 2, 1, 2, 3}
+}
+
+func circle(x float32, y float32, radius float32, clr color.RGBA) ([]ebiten.Vertex, []uint16) {
+	var path vector.Path
+
+	path.MoveTo(x, y)
+	path.Arc(x, y, radius, 0, math.Pi*2, vector.Clockwise)
+
+	vertices, indices := path.AppendVerticesAndIndicesForFilling(nil, nil)
+
+	for i := range vertices {
+		vertices[i].ColorR = float32(clr.R) / 255
+		vertices[i].ColorG = float32(clr.G) / 255
+		vertices[i].ColorB = float32(clr.B) / 255
+		vertices[i].ColorA = float32(clr.A) / 255
+	}
+
+	return vertices, indices
+
+}
+
+func line(x0, y0, x1, y1, width float32, clr color.RGBA) ([]ebiten.Vertex, []uint16) {
+
+	theta := math.Atan2(float64(y1-y0), float64(x1-x0))
+	theta += math.Pi / 2
+	dx := float32(math.Cos(theta))
+	dy := float32(math.Sin(theta))
+
+	r := float32(clr.R) / 0xff
+	g := float32(clr.G) / 0xff
+	b := float32(clr.B) / 0xff
+	a := float32(clr.A) / 0xff
+
+	return []ebiten.Vertex{
+		{
+			DstX:   x0 - width*dx/2,
+			DstY:   y0 - width*dy/2,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+		{
+			DstX:   x0 + width*dx/2,
+			DstY:   y0 + width*dy/2,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+		{
+			DstX:   x1 - width*dx/2,
+			DstY:   y1 - width*dy/2,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+		{
+			DstX:   x1 + width*dx/2,
+			DstY:   y1 + width*dy/2,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+	}, []uint16{0, 1, 2, 1, 2, 3}
+}
+
+func triangle(x, y, w, h float32, clr color.RGBA) ([]ebiten.Vertex, []uint16) {
+	r := float32(clr.R) / 0xff
+	g := float32(clr.G) / 0xff
+	b := float32(clr.B) / 0xff
+	a := float32(clr.A) / 0xff
+	x0 := x
+	y0 := y
+	x1 := x + w
+	y1 := y + h/2
+	y2 := y + h
+
+	return []ebiten.Vertex{
+		{
+			DstX:   x0,
+			DstY:   y0,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+		{
+			DstX:   x1,
+			DstY:   y1,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+		{
+			DstX:   x0,
+			DstY:   y2,
+			SrcX:   1,
+			SrcY:   1,
+			ColorR: r,
+			ColorG: g,
+			ColorB: b,
+			ColorA: a,
+		},
+	}, []uint16{0, 1, 2}
+}
