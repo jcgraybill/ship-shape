4886f73ad7c1c613229e1682a5067562d7115c19
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2022-04-21 23:17:06 -0700

    balance tweaks

--- ui/shapes.go
+++ ui/shapes.go
@@ -29,60 +29,6 @@ func init() {
 
 }
 
-func Rectangle(x, y, w, h float32, clr color.RGBA) ([]ebiten.Vertex, []uint16) {
-	r := float32(clr.R) / 0xff
-	g := float32(clr.G) / 0xff
-	b := float32(clr.B) / 0xff
-	a := float32(clr.A) / 0xff
-	x0 := x
-	y0 := y
-	x1 := x + w
-	y1 := y + h
-
-	return []ebiten.Vertex{
-		{
-			DstX:   x0,
-			DstY:   y0,
-			SrcX:   1,
-			SrcY:   1,
-			ColorR: r,
-			ColorG: g,
-			ColorB: b,
-			ColorA: a,
-		},
-		{
-			DstX:   x1,
-			DstY:   y0,
-			SrcX:   1,
-			SrcY:   1,
-			ColorR: r,
-			ColorG: g,
-			ColorB: b,
-			ColorA: a,
-		},
-		{
-			DstX:   x0,
-			DstY:   y1,
-			SrcX:   1,
-			SrcY:   1,
-			ColorR: r,
-			ColorG: g,
-			ColorB: b,
-			ColorA: a,
-		},
-		{
-			DstX:   x1,
-			DstY:   y1,
-			SrcX:   1,
-			SrcY:   1,
-			ColorR: r,
-			ColorG: g,
-			ColorB: b,
-			ColorA: a,
-		},
-	}, []uint16{0, 1, 2, 1, 2, 3}
-}
-
 func Circle(x float32, y float32, radius float32, clr color.RGBA) ([]ebiten.Vertex, []uint16) {
 	var path vector.Path
 
