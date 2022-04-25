9d348167da864664a386721002f91eb5e260a190
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2022-04-25 13:17:39 -0700

    upgrade ebiten to 2.3.1

--- main.go
+++ main.go
@@ -52,7 +52,8 @@ func main() {
 	}
 
 	ebiten.SetWindowSize(ui.WindowW, ui.WindowH)
-	ebiten.SetWindowResizable(true)
+
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
 	g.load(level.StartingLevel())
 	ui.InitializeShader()
